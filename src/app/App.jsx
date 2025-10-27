b39ab61425a2b70a28c2041c0ce098d62fc4b55a
yousefmobelal <ym8370302@gmail.com>
2025-10-27 19:34:45 +0300

    import ProductDetailsPage

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "../components/Navbar";
 import SubNav from "../components/SubNav";
+import ProductDetailsPage from "../pages/productDetails/ProductDetailsPage";
 
 function App() {
   return (
@@ -11,5 +12,4 @@ function App() {
     </>
   );
 }
-
 export default App;
